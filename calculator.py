cdc79d9a9f40dbacdbde5aae45d305169681d73b
KESHAVKAUSHIK2005 <keshakaushik18rcb@gmail.com>
2020-12-22 14:28:38 +0530

    4 operations completed

---
+++ calculator.py
@@ -0,0 +1,13 @@
+a=input("enter 1st number")
+b=input("enter 2nd number")
+
+op=input("enter opr")
+if op=="+" :
+    print(float(a)+float(b))
+elif op=="-":
+    print(float(a)-float(b))
+elif op=="*":
+    print(float(a)*float(b))
+elif op=="/":
+    print(float(a)/float(b))
+ 
